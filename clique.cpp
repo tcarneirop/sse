e9724b83fd6d3dab0db32e3b4e0ebe050dc591c7
Tiago Carneiro <carneirouece@gmail.com>
2014-05-07 01:53:09 -0300

    Corrigi uns erros de indice quando vcorrent maior que 64, porem temos erro no and.

--- clique.cpp
+++ clique.cpp
@@ -27,7 +27,7 @@
 using namespace std;
 
 
-
+#define ULLONG 64
 #define N 8
 
 #define qtd_vertices 8
@@ -59,7 +59,7 @@ void displayBits(unsigned value){
 
 
 
-	cout<< setw(7) << value << " = ";
+	//cout<< setw(7) << value << " = ";
 
 	for (unsigned c = 1; c <= SHIFT +1; c++){
 		cout << (value & MASK ? '1' : '0');
@@ -78,7 +78,7 @@ void displayBitsll(unsigned long long value){
 	const unsigned long long  SHIFT = 8 * sizeof( unsigned long long ) - 1;
 	const unsigned long long MASK = (unsigned long long)1 << SHIFT ;// joga do menos para o mais significativo
 
-	cout<< setw(7) << value << " = ";
+	//cout<< setw(7) << value << " = ";
 
 	for (unsigned long long c = 1; c <= SHIFT +1; c++){
 		cout << (value & MASK ? '1' : '0');
@@ -107,29 +107,6 @@ void preencherGrafoll(){
 	//G(i,j) = (unsigned long long)8 + MASK + 4096 + 65536 + 8589934592;
 }
 
-
-//inline int qtdZerosLinhall(int linha, unsigned long long *GRAFO, int *primeiraColuna){
-//
-//	int qtd = 0;
-//	int retorno;
-//
-//	bool primeiraVez = true;
-//
-//	for(int coluna = 0; coluna<qtd_palavrasULL; ++coluna){
-//		retorno = __builtin_popcountll(GRAFO[linha*N+coluna]);
-//		if(primeiraVez == true && (retorno > 0)){
-//			cout<<"\n\tcoluna "<< coluna;
-//			cout<<"\nRerotno "<<retorno<<"\n";
-//			*primeiraColuna = coluna;
-//			primeiraVez = false;
-//			cout<<"\n\tPrimeira coluna com 1(uns): "<<*primeiraColuna<<" \n";
-//
-//		}
-//		qtd += retorno;
-//	}
-//	return qtd;
-//
-//}
 
 
 int isZero(unsigned long long *R, int i,int *j){
@@ -171,25 +148,37 @@ void imprimirGrafoll(){
 	}
 }
 
+void imprimirGrafoll(unsigned long long *G){
 
-void teste(unsigned long long *R, int i){
-
-	int palavraPrimeiroUm, j;
-
-
-	do{
-
-		palavraPrimeiroUm=isZero(R,i,&j);
-		displayBitsll(R(i,palavraPrimeiroUm));
-		//cout<<"\nColuna primeiro um: "<< palavraPrimeiroUm<<".\n"<<"Coluna j: "<<j<<". \n";
-		//cout<<"Imprimindo linha antes do shift: ";
-		//imprimirLinhall(R,i);
-		R(i,palavraPrimeiroUm) = R(i,palavraPrimeiroUm) & ~((unsigned long long)1<<j); //retirando o um da posicao j
-		//cout<<"Imprimindo linha apos o shift: ";
-		displayBitsll(R(i,palavraPrimeiroUm));
-
+	cout<<"\n\nImprimindo Grafo "<<N<<" x "<<N<<" long long. \n";
+	for(int i = 0; i<qtd_vertices; ++i){
+		cout<<"\nLinha "<< i<< " : ";
+		cout<<"\n";
+		for(int j =0; j<qtd_palavrasULL;++j){
+			cout<<"\n Coluna "<< j<< " : ";
+			displayBitsll(G(i,j));
+		}
 	}
-	while(R(i,palavraPrimeiroUm)>0);
+}
+
+
+
+void teste(unsigned long long *R){
+
+	int palavraPrimeiroUm;
+	int j;
+	for(int i = 0; i<qtd_vertices;++i){
+
+		do{
+			palavraPrimeiroUm = isZero(R,i,&j);
+			if(palavraPrimeiroUm>-1){
+				cout<<"\nVertice "<< i<<". \nPrimeira palavra ULL contendo um: "<<palavraPrimeiroUm<<" . Posiçao: "<<j<<" .\n";
+				R(i,palavraPrimeiroUm) = R(i,palavraPrimeiroUm) & ~(1ULL<<j);
+			}
+		}
+		while(palavraPrimeiroUm> -1);
+	}
+
 
 }
 
@@ -201,15 +190,15 @@ void inicializarVetorCll(){
 
 
 
+
 void cliqueNormal(){
 
-	/*@TODO ok
-	 * -setar a raiz do espaco de solucoes na clique. Nao fazia sentido nao ter isso;
-	 * -substituor qtd_palavrasULL por qtd_palavrasULL
-	 * -substituir palavraPrimeiroUm por palavraPrimeiroUm;
-	 * -mostrar a clique e o size da clique ao sair do loop mais interno;
-	 * -Obs: ao sair do laco mais interno eu obtenho uma clique. Faco c[]=c[]&~(1<<pilha[i]);--i.
-	 * -colocar um for para realizar a busca para todo no'.
+	/*TODO:
+	 * Lembrando que isso so' serve se o grafo tiver ate' 63 vertices, algum problema com os ands...
+	 * 	-Corrigido o problema do c[]=c[] &~ do fim (OK);
+	 * 	-Corrigido o problema do c[] =c[] | verticeCorrente,quando vc > 64;
+	 *
+	 * 	??E os ands?
 	 *
 	 * */
 
@@ -219,63 +208,110 @@ void cliqueNormal(){
 	int palavraPrimeiroUm;
 	int pilha[qtd_vertices];
 	int palavraUtilizada[qtd_vertices];
-	int verticeCorrente = 0;
 	int qtd_cliques = 0;
+	int palavraRaiz;
+	int verticeEmJ;
+	int posicaoRaiz;
 
 
 	for(int verticeCorrente = 0; verticeCorrente<qtd_vertices;++verticeCorrente){
 
+		i=0;
+
+		inicializarVetorCll();
+
+		palavraRaiz  = (int)(verticeCorrente/ULLONG);
+		posicaoRaiz = verticeCorrente % ULLONG;
 
 		cout<<"\nVertice corrente: "<<verticeCorrente<<"\n";
-		i=0;
+
+
 		for(j = 0; j<qtd_palavrasULL;++j)
 			R(i,j) = G(verticeCorrente,j);
 
 
-
-		inicializarVetorCll();
-
-
+		c[palavraRaiz] = c[palavraRaiz] | (1ULL<<posicaoRaiz);
 
 		while(i>=0){
 
 			palavraPrimeiroUm=isZero(R,i,&j);
 
-			c[palavraPrimeiroUm] = c[palavraPrimeiroUm] | (1ULL<<verticeCorrente); //???? isso tem que ser o tempo todo verificado? creio que nao...
-
-			//	cout<<"\n\nLoop externo\n";
-			//	cout<<"\n i : "<<i<<"\nColuna primeiro um: "<< palavraPrimeiroUm<<".\n"<<"Coluna j: "<<j<<". \n";
-			//	cout<<"R("<<i<<"): \n";
-			//	displayBitsll(R(i,palavraPrimeiroUm));
+			//???? isso tem que ser o tempo todo verificado? creio que nao...
 
 
 			while(palavraPrimeiroUm>=0){
 
+
+				//	cout<<"\n\nLoop externo\n";
+				cout<<"\n i : "<<i<<"\nPalavra primeiro um: "<< palavraPrimeiroUm<<".\n"<<"Coluna j: "<<j<<". \n";
+
+
 				palavraUtilizada[i] = palavraPrimeiroUm;
 
 				c[palavraPrimeiroUm] = c[palavraPrimeiroUm] | (1ULL<<j);
+
 				pilha[i] = j;
 
+
 				//cout<<"\nC: \n";
-				//displayBitsll(c[0]);
+//				displayBitsll(c[0]);
+//				displayBitsll(c[1]);
+
+
+
+				/*
+				 * eis um erro... coluna j. Se a palavra > 0, j= palavra*64+j
+				 * */
+				verticeEmJ = palavraPrimeiroUm*64+j;
+				/*
+				 * Ex: se nao tivermos isso teremos problema na etapa R(i+1) = R(i) & G(j)
+				 * ???: que outro erro temos aqui?
+				 * Apos o for, mesmo sem realizar o andnot, R(i) torna-se nula.
+				 * ?????
+				 * */
+				/*??????????????????????????????????????????????????????????????
+				 * O acima da ok, o abaixo zera todos as posicoes do vetor R
+				 * */
+
+
+
+//				cout<<"\n\n\nR("<<i<<",0): \n";
+//				displayBitsll(R(0,0));
+//				cout<<"R("<<i<<",1): \n";
+//				displayBitsll(R(0,1));
+//
+//				cout<<"G("<<verticeEmJ<<",0): \n";
+//				displayBitsll(G(verticeEmJ,0));
+//				cout<<"G("<<verticeEmJ<<",1): \n";
+//				displayBitsll(G(verticeEmJ,1));
+//
+//				cout<<"R("<<i+1<<",0): \n";
+//				displayBitsll(R(0,0)&G(verticeEmJ,0));
+//				cout<<"R("<<i+1<<",1): \n";
+//				displayBitsll(R(0,1)&G(verticeEmJ,1));
 
 
 				for(int col = 0; col<qtd_palavrasULL;++col)
-					R(i+1,col) = R(i,col) & G(j,col);
+					R(i+1,col) = R(i,col) & G(verticeEmJ,col);
 
-				///R(i+1,0) = R(i,0) & G(j,0);
-				//R(i+1,1) = R(i,1) & G(j,1);
-				//R(i+1,2) = R(i,2) & G(j,2);
-				//R(i+1,3) = R(i,3) & G(j,3);
 
+				//!!!!Tudo em R foi zerado! õ0
+
+
+//				imprimirGrafoll(R);
+//				exit(1);
 
 				R(i,palavraPrimeiroUm) = R(i,palavraPrimeiroUm) & ~(1ULL<<j); //retirando o um da posicao j
 
 
-				//cout<<"R("<<i<<"): \n";
-				//displayBitsll(R(i,palavraPrimeiroUm));
-				//cout<<"R("<<i+1<<"): \n";
-				//displayBitsll(R(i+1,palavraPrimeiroUm));
+//				cout<<"R("<<i<<"): \n";
+//				displayBitsll(R(i,palavraPrimeiroUm));
+//
+//				cout<<"R("<<i+1<<",0): \n";
+//				displayBitsll(R(i+1,0));
+//				cout<<"R("<<i+1<<",1): \n";
+//				displayBitsll(R(i+1,1));
+//
 
 				i++;
 				//colunaIteracaoAnterior = palavraPrimeiroUm;
@@ -288,6 +324,7 @@ void cliqueNormal(){
 				++qtd_cliques;
 				cout<<"\nClique "<<qtd_cliques<<" : \n";
 				displayBitsll(c[0]);
+				displayBitsll(c[1]);
 				c[palavraUtilizada[i]] = c[palavraUtilizada[i]] & ~(1ULL<<pilha[i]);} //algum condicional aqui? estamos mantendo o j antigo...
 
 
@@ -295,25 +332,29 @@ void cliqueNormal(){
 		}
 		//cout<<"\nAo fim: \n";
 		//displayBitsll(c[0]);
-		c[0] = c[0] & ~(1ULL<<verticeCorrente); //pois todo espaco de solucoes cuja origem eh ''vertice'' foi verificado
+		c[palavraRaiz] = c[palavraRaiz] & ~(1ULL<<verticeCorrente); //pois todo espaco de solucoes cuja origem eh ''vertice'' foi verificado
 
 
 
+		/*@TODO: OK, verificar...
+		 *
+		 *Creio que também exista um erro aqui. se o vertice for 64, nao iremos mandar andar 64 posicoes. erro...
+		 *
+		 **/
 
 		for(int k = 0; k<qtd_vertices;++k)
-			G(k,verticeCorrente) = G(k,verticeCorrente) & ~(1ULL<<verticeCorrente);
+			G(k,palavraRaiz) = G(k,palavraRaiz) & ~(1ULL<<posicaoRaiz);
 
 
 	}
 	cout<<"\n\nQtd_cliques: "<<qtd_cliques<<"\n";
 }
 
-/*
- * Lembrando que isso so' serve se o grafo tiver ate' 63 vertices
- * */
+
 
 void criarGrafoControle(){
 
+	// n = 8, cliques = 19
 	for(int i = 0; i<qtd_vertices; ++i)
 		for(int j = 0; j<qtd_palavrasULL;++j)
 			G(i,j) = (unsigned long long)0;
@@ -359,19 +400,24 @@ void criarGrafoControle(){
 
 void criarGrafoControleDois(){
 
+	// n = 4, cliques = 11
+
 	for(int i = 0; i<qtd_vertices; ++i)
 		for(int j = 0; j<qtd_palavrasULL;++j)
 			G(i,j) = (unsigned long long)0;
 
 
 	G_line(0)= G_line(0) | (1ULL<<1);
+	G_line(0)= G_line(0) | (1ULL<<2);
 	G_line(0)= G_line(0) | (1ULL<<3);
 
 
 	G_line(1)= G_line(1) | (1ULL);
+	G_line(1)= G_line(1) | (1ULL<<2);
 	G_line(1)= G_line(1) | (1ULL<<3);
 
 	G_line(2)= G_line(2) | (1ULL);
+	G_line(2)= G_line(2) | (1ULL<<1);
 	G_line(2)= G_line(2) | (1ULL<<3);
 
 	G_line(3)= G_line(3) | (1ULL);
@@ -380,11 +426,34 @@ void criarGrafoControleDois(){
 
 }
 
+
+void criarGrafoControleTres(){
+
+	for(int i = 0; i<qtd_vertices; ++i)
+		for(int j = 0; j<qtd_palavrasULL;++j)
+			G(i,j) = (unsigned long long)0;
+
+
+	G(63,1)= G(63,1) | (1ULL); //63-64
+	G(63,1)= G(63,1) | (1ULL<<1); //63-65
+
+	G(64,0)= G(64,0) | (1ULL<<63);
+	G(64,1)= G(64,1) | (1ULL<<1); //64-65
+
+	G(65,0)= G(65,0) | (1ULL<<63);
+	G(65,1)= G(65,1) | (1ULL); //64-65
+
+
+}
+
+
 int main(){
 
-	cout<<"\n"<<sizeof(int)<<"\n"<<sizeof(unsigned long long)<<"\n";
-
 	//preencherGrafoll();
+	//criarGrafoControleTres();
+	//criarGrafoControleDois();
+
+	cout<<"\n\nQuantidade de vertices: "<<qtd_vertices<<" . Quantidade de palavras ULL: "<<qtd_palavrasULL<<" .\n";
 	criarGrafoControle();
 	imprimirGrafoll();
 
@@ -396,6 +465,8 @@ int main(){
 
 	//cout<<"\n\nClique"<<endl;
 
+	//teste(G);
 	cliqueNormal();
+
 	return 0;
 }
